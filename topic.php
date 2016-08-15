5062de15d77841e1699211d85f214eed469ae202
Arinerron <Arinerron@users.noreply.github.com>
2016-08-15 12:32:26 -0700

    Update topic.php

--- topic.php
+++ topic.php
@@ -6,9 +6,11 @@ List posts from a topic
 
 include 'header.php';
 include 'includes/connect.php';
+include 'includes/psl-config.php';
+include 'includes/strings.php';
 include 'includes/query-functions.php';
 
-date_default_timezone_set('America/Toronto');
+date_default_timezone_set(TIMESTAMP);
 
 $query = "SELECT topic_id, topic_subject FROM topics WHERE topic_id=?";
 $stmt = $connect->prepare($query);
@@ -17,13 +19,13 @@ $stmt->execute();
 $result = $stmt->get_result();
 
 if(!$stmt) {
-    echo 'There was a problem showing the topic, please try again later.';
+    echo ERROR_CONNECTION_FAILED;
 } else {
     $topic_id = $_GET['topic_id'];
     $numrows = $result->num_rows;
 
     if($numrows == 0) {
-        echo 'This topic does not exist.';
+        echo MESSAGE_TOPIC_NONEXISTANT;
     } else {
 
         echo '<div class="container">';
@@ -84,28 +86,28 @@ if(!$stmt) {
         $result = $stmt->get_result();
 
         if(!$stmt) {
-            echo 'There was an error showing the posts.  Please try again later.';
+            echo ERROR_CONNECTION_FAILED;
         } else {
             $numrows = $result->num_rows;
 
             if($numrows == 0) {
-                die('There are no posts on this page.');
+                die(MESSAGE_TOPIC_NONE); // later, redirect to last page of posts.
             } else {
 
                 //Determine
-                if($totalPosts == 1) $posts = 'post';
-                else $posts = 'posts';
+                if($totalPosts == 1) $posts = SHORT_POST_SINGULAR;
+                else $posts = SHORT_POST_PLURAL;
 
                 echo '<div class="status-bar post-header">
-                        <p>' . $totalPosts . ' ' . $posts . ' in this topic</p>
+                        <p>' . str_replace('%noun%', $posts, str_replace('%posts%', $totalPosts, MESSAGE_TOPIC_POSTS)) . '</p>
                       </div>';
                 echo '<div class="topic-box">';
 
                 while($row = $result->fetch_assoc()) {
 
                     //Get user title
-                    if($row['user_level'] == 0) $user_level = 'Member';
-                    else $user_level = "Admin";
+                    if($row['user_level'] == 0) $user_level = SHORT_USER_MEMBER;
+                    else $user_level = SHORT_USER_ADMIN;
 
                     echo '<div class="post">
                             <div class="mobile-profile-info">
@@ -117,9 +119,9 @@ if(!$stmt) {
                               <p class="big-text black"><strong>' . $row['user_name'] . '</strong></p>
                               <p class="small-text gray">' . $user_level . '</p>
                               <div class="profile-picture small center" style="background-image: url(/assets/profile-pictures/' . $row['user_icon']. ')"></div>
-                              <p class="tiny-text gray">' . getUserPosts($row['user_id']) . ' posts</p>
+                              <p class="tiny-text gray">' . str_replace('%posts%', '' . getUserPosts($row['user_id']), MESSAGE_USER_POSTS) . '</p>
                             </div><div class="post-content">
-                              <p class="tiny-text gray">Posted on ' . date('j F, Y', strtotime($row['post_date'])) . ' at ' . date('g:i A', strtotime($row['post_date'])) . '</p>
+                              <p class="tiny-text gray">' . str_replace('%time%', '' . date('g:i A', strtotime($row['post_date'])), str_replace('%date%', '' . date('j F, Y', strtotime($row['post_date'])), MESSAGE_TOPIC_DATE)) . '</p>
                               <div class="tiny-text black">' . $row['post_content'] . '</div>
                             </div>
                           </div>';
@@ -142,7 +144,7 @@ if(!$stmt) {
 
             if(!isset($_SESSION['signed_in']) && !$_SESSION['signed_in']) {
                 echo '<div class="container">
-                        <p class="small-text gray">You must be <a href="/signin.php">signed in</a> to reply.</p>
+                        <p class="small-text gray">' . MESSAGE_REPLY_SIGNOUT . '</p>
                       </div>';
             } else {
 
@@ -172,7 +174,7 @@ if(!$stmt) {
                           <textarea id="editor" autocomplete="off" name="reply-content"></textarea>
                           <input type="hidden" name="topic-id" value="' . $_GET['topic_id'] . '">
                           <p class="error-noenter tiny-text red"></p>
-                          <input id="submit" class="button red normal" type="submit" value="Reply" />
+                          <input id="submit" class="button red normal" type="submit" value="' . SHORT_REPLY_BUTTON . '" />
                         </form>
                       </div>';
             }
