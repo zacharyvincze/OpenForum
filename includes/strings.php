9484f76dd4fe9bc1dc4a11939b1bcd8039f33462
Arinerron <Arinerron@users.noreply.github.com>
2016-08-15 10:59:32 -0700

    Update strings.php

--- includes/strings.php
+++ includes/strings.php
@@ -8,7 +8,7 @@ define('ERROR_CATEGORY_NONEXISTANT', 'That category doesn\'t exist.');
 
 
 // Message Strings
-define('MESSAGE_WELCOME_STRINGS', 'Welcome!/Aloha!/Hello!/Welcome back!/Hey!/Hi!/Bonjour!/Howdy!'); // randomly chosen from the list of strings separated by '/'
+define('MESSAGE_WELCOME_STRINGS', 'Welcome!/Aloha!/Hello!/Welcome back!/Hey!/Hi!/Bonjour!/Howdy!'); // andomly chosen from the list of strings separated by '/'
 define('MESSAGE_NOT_REGISTERED', 'Haven\'t registered yet?  Do it <a href="signup.php">here</a>!');
 define('MESSAGE_WELCOME_SIGNUP', 'Welcome to the site.  Just fill in the fields and you should be good to go!');
 define('MESSAGE_CATEGORY_EMPTY', 'There are no topics in this category yet.');
@@ -25,10 +25,18 @@ define('MESSAGE_TOPIC_EMPTY', 'All the fields must be filled in.');
 
 
 // Short Strings
+define('SHORT_USER_SIGNIN', 'Sign In');
+define('SHORT_USER_REGISTER', 'Register');
+define('SHORT_USER_USERNAME', 'Username');
+define('SHORT_USER_PASSWORD', 'Password');
+define('SHORT_USER_DESCRIPTION', 'Edit Profile');
+define('SHORT_USER_OVERVIEW', 'Profile Overview');
+define('SHORT_USER_SIGNOUT', 'Sign Out');
 define('SHORT_CATEGORY_CREATE', 'Create a Category');
 define('SHORT_CATEGORY_NAME', 'Category name');
 define('SHORT_CATEGORY_ADD', 'Add Category');
 define('SHORT_TOPIC_CREATE', 'Create a Topic');
 define('SHORT_TOPIC_BUTTON', 'Create Topic');
 define('SHORT_TOPIC_SUBJECT', 'Topic subject');
+define('SHORT_NAV_HOME', 'Home');
 ?>
