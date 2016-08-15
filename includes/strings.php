b77a732eda720da37436542ede8de5f2eca8c96b
Arinerron <Arinerron@users.noreply.github.com>
2016-08-15 09:46:55 -0700

    Create strings.php file
    This file contains all of the strings used on the website (not right now, but later it will). This is a start so there is multi-language support later. :P

---
+++ includes/strings.php
@@ -0,0 +1,15 @@
+<?php
+// This file contains all of the strings displayed on the site. Later there will be more strings, and the strings will actually do something :P
+
+// Error Strings
+define('ERROR_CONNECTION_FAILED', 'Error: Connection to database failed.');
+define('ERROR_VERIFICATION_FAILED', 'Error: Email verification failed.');
+define('ERROR_INVALID_ACCESS', 'Error: File cannot be accessed directly');
+define('ERROR_NONEXISTANT_USER', 'That user doesn\'t exist.');
+
+
+// Message Strings
+define('MESSAGE_WELCOME_STRINGS', 'Welcome!/Aloha!/Hello!/Welcome back!/Hey!/Hi!/Bonjour!'); // andomly chosen from the list of strings separated by '/'
+define('MESSAGE_NOT_REGISTERED', 'Haven\'t registered yet?  Do it <a href="signup.php">here</a>!');
+define('MESSAGE_WELCOME_SIGNUP', 'Welcome to the site.  Just fill in the fields and you should be good to go!');
+?>
