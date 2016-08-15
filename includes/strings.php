34ed29c18d37aebb1a424025445e85ffce97d6df
Arinerron <Arinerron@users.noreply.github.com>
2016-08-15 11:58:25 -0700

    Update strings.php

--- includes/strings.php
+++ includes/strings.php
@@ -9,9 +9,10 @@ define('ERROR_USER_NONEXISTANT', 'That user doesn\'t exist.');
 // Message Strings
 define('MESSAGE_WELCOME_STRINGS', 'Welcome!/Aloha!/Hello!/Welcome back!/Hey!/Hi!/Bonjour!/Howdy!'); // andomly chosen from the list of strings separated by '/'
 define('MESSAGE_NOT_REGISTERED', 'Haven\'t registered yet?  Do it <a href="signup.php">here</a>!');
-define('MESSAGE_WELCOME_SIGNUP', 'Welcome to the site.  Just fill in the fields and you should be good to go!');
+define('MESSAGE_WELCOME_SIGNUP', 'Welcome to the site.  Just fill in the fields and you\'re good to go!');
 define('MESSAGE_USER_NONEXISTANT', 'That user doesn\'t exist.');
 define('MESSAGE_USER_SENT', 'Email was sent.');
+define('MESSAGE_USER_SIGNIN', 'You are already signed in, you can <a href="/signout.php">sign out</a> if you want to switch users.');
 define('MESSAGE_CATEGORY_EMPTY', 'There are no topics in this category yet.');
 define('MESSAGE_CATEGORY_SIGNOUT', 'Admins must be <a href="signin.php">signed in</a> to create a category.');
 define('MESSAGE_CATEGORY_UNAUTHORIZED', 'You must be an admin to create a category.');
@@ -22,7 +23,7 @@ define('MESSAGE_CATEGORY_EMPTY', 'All the fields must be filled in.');
 define('MESSAGE_TOPIC_SIGNOUT', 'You must be <a href="signin.php">signed in</a> to create a topic.');
 define('MESSAGE_TOPIC_CATEGORY', 'You have not created any categories yet.');
 define('MESSAGE_TOPIC_SUPERCATEGORY', 'Before you can post a topic, an admin must create a category.');
-define('MESSAGE_TOPIC_SUCCESS', 'You have successfully created <a href="topic.php?topic_id=%topic_id%&page=1">your new topic</a>.'); // %topic_id% is replaced with 
+define('MESSAGE_TOPIC_SUCCESS', 'You have successfully created <a href="topic.php?topic_id=%topic_id%&page=1">your new topic</a>.'); // %topic_id% is replaced with the id
 define('MESSAGE_TOPIC_EMPTY', 'All the fields must be filled in.');
 define('MESSAGE_REPLY_SIGNOUT', 'You must be <a href="/login">signed in</a> to post a reply.');
 define('MESSAGE_REPLY_EMPTY', 'All the fields must be filled in.');
@@ -30,16 +31,19 @@ define('MESSAGE_REPLY_EMPTY', 'All the fields must be filled in.');
 
 // Short Strings
 define('SHORT_USER_SIGNIN', 'Sign In');
+define('SHORT_USER_LOGIN', 'Login');
+define('SHORT_USER_SIGNUP', 'Sign Up');
 define('SHORT_USER_REGISTER', 'Register');
 define('SHORT_USER_USERNAME', 'Username');
 define('SHORT_USER_PASSWORD', 'Password');
+define('SHORT_USER_CONFIRM', 'Confirm Password');
+define('SHORT_USER_EMAIL', 'Email');
+define('SHORT_USER_EITHER', 'Username or Email');
 define('SHORT_USER_DESCRIPTION', 'Edit Profile');
 define('SHORT_USER_OVERVIEW', 'Profile Overview');
 define('SHORT_USER_SIGNOUT', 'Sign Out');
 define('SHORT_USER_POSTS', 'Post Count');
 define('SHORT_USER_JOINED', 'Joined');
-define('SHORT_USER_', '');
-define('SHORT_USER_', '');
 define('SHORT_CATEGORY_CREATE', 'Create a Category');
 define('SHORT_CATEGORY_NAME', 'Category name');
 define('SHORT_CATEGORY_ADD', 'Add Category');
