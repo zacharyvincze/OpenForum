7cac36208169883d179d69980f11b877b8e0fcff
zacharyvincze <zvincze@live.com>
2016-08-16 14:56:18 -0400

    Topic deletion and more theming

--- delete_topic.php
+++ delete_topic.php
@@ -1,6 +1,9 @@
 <?php
 
 include 'includes/connect.php';
+include 'includes/functions.php';
+
+sec_session_start();
 
 date_default_timezone_set(TIMESTAMP);
 
@@ -11,8 +14,8 @@ $stmt->execute();
 $result = $stmt->get_result();
 if(!$stmt) {
     echo ERROR_CONNECTION_FAILED;
-} else if(!$_SESSION['signed_in']) { 
-    echo 'signed_in';
+} else if(!$_SESSION['signed_in']) {
+    echo 'You are not signed in.';
 } else if($_SERVER['REQUEST_METHOD'] != 'POST') {
     echo ERROR_INVALID_ACCESS;
 } else if(!isset($_POST['csrf_token']) || $_SESSION['csrf_token'] != $_POST['csrf_token']) {
@@ -24,13 +27,13 @@ if(!$stmt) {
         echo MESSAGE_TOPIC_NONEXISTANT;
     } else {
         while($row = $result->fetch_assoc()) {
-            if($_SESSION['user_level'] == 1 && DEVELOPMENT_MODE && $_SESSION['user_id'] == $row['user_id']) {
-                $query = "UPDATE topics SET topic_visible='FALSE' WHERE topic_id=?"; // It doesn't *actually* delete them, just hides them.
+            if($_SESSION['user_level'] == 1 && DEVELOPMENT_MODE || $_SESSION['user_id'] == $row['user_id']) {
+                $query = "DELETE FROM topics WHERE topic_id=?"; // It doesn't *actually* delete them, just hides them.
                 $stmt = $connect->prepare($query);
                 $stmt->bind_param('i', $_POST['topic_id']);
                 $stmt->execute();
-                
-                echo "success";
+
+                echo "true";
             } else {
                 echo 'unauthorized';
             }
