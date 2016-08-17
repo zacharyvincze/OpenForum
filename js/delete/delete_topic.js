aed3b27cdf9fd2b8546b40ce78e03a0b9e7abab7
Arinerron <Arinerron@users.noreply.github.com>
2016-08-16 20:47:37 -0700

    Update delete_topic.js

--- js/delete/delete_topic.js
+++ js/delete/delete_topic.js
@@ -12,7 +12,7 @@ function deleteTopic(topic_id, user_id, csrf_token, topic_cat) {
             if(html == 'true') {
                 window.location.replace('/category.php?cat_id=' + topic_cat);
             } else {
-                alert('Fail!');
+                alert(html);
             }
         }
 
