019d25d151c463630f92a279ed99500b4cc12bb7
Arinerron <arinesau@gmail.com>
2016-09-30 22:12:38 -0700

    Add description to user profiles

--- public_html/profile.php
+++ public_html/profile.php
@@ -42,7 +42,7 @@ if($numrows == 0) {
 
     //Basic user page setup
     echo '<div class="profile-banner faded-color">
-            <img class="profile-picture inverted-color big" src="/assets/profile-pictures/'.$user_icon.'">
+            <img class="profile-picture inverted-color big" src="/assets/profile-pictures/'.$user_icon.'">' . htmlspecialchars($user_about) . '
           </div>
           <div class="profile-statistics primary-color">
             <div class="posts">
